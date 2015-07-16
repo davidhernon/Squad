9db41bddb18a84698a9694cf89e0febd16128c7a
davidhernon <david.hernon@mail.mcgill.ca>
2015-07-16 10:42:09 -0400

    Initial Commit

---
+++ Ecstatic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ecstatic
+//
+//  Created by David Hernon on 2015-07-16.
+//  Copyright (c) 2015 David Hernon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Ecstatic/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Ecstatic
+//
+//  Created by David Hernon on 2015-07-16.
+//  Copyright (c) 2015 David Hernon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
